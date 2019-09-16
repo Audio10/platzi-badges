30e70d53797c928a7498af950f980e8fb78df347
Audio10 <claudiojesus_10@hotmail.com>
2019-09-15 22:59:10 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// import './global.css';
+// import App from './components/App';
+
+// const container = document.getElementById('app');
+
+// ReactDOM.render(<App />, container);
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const element = <h1>Hello, Platzi Badges !</h1>
+
+const container = document.getElementById('app')
+
+ReactDom.render(element, container)
